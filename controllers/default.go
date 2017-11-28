55220bc9f738c9ce92f8db90d42afee49cab3e7f
Deju Liu <cheneylew@163.com>
2017-11-28 18:35:51 +0800

    login page!

--- controllers/default.go
+++ controllers/default.go
@@ -27,3 +27,14 @@ func (c *MainController) UserLogin() {
 	c.TplName = "user_login.html"
 }
 
+// @router /user/regist/ [get]
+func (c *MainController) UserRegist() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "user.html"
+
+	c.Layout = "layout.html"
+	c.TplName = "user_regist.html"
+}
+
+
